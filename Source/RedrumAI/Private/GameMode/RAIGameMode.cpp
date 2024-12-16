9fe1b9b7d3a338f6b6d6dbdec940162349245947
YJ_MSI <nichy98@naver.com>
2024-12-16 15:47:01 +0900

    [add] : 초기 프로젝트 구조 및 OperaHouse 애셋 추가 [1/3]

---
+++ Source/RedrumAI/Private/GameMode/RAIGameMode.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameMode/RAIGameMode.h"
+//Secrets.ini 경로 탐지 및 ini로부터 읽어오는 헤더
+#include "Misc/ConfigCacheIni.h"
+#include "Misc/Paths.h"
+#include "HAL/PlatformFileManager.h"
+
+//Secretes.ini로부터 API_KEY 불러오는 예시코드
+ARAIGameMode::ARAIGameMode()
+{
+	// Secrets.ini 경로 지정
+	FString SecretsPath = FPaths::Combine(FPaths::ProjectConfigDir(), TEXT("Secrets.ini"));
+	GConfig->LoadFile(SecretsPath);
+
+	FString OpenAI_API_Key;
+	if (GConfig->GetString(TEXT("OpenAI"), TEXT("OpenAI_API_KEY"), OpenAI_API_Key, SecretsPath))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("OpenAI_API_KEY Loaded: %s"), *OpenAI_API_Key);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to load OpenAI_API_KEY from Secrets"));
+	}
+
+	FString HF_API_Key;
+	if (GConfig->GetString(TEXT("HuggingFace"), TEXT("HF_API_KEY"), HF_API_Key, SecretsPath))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HF_API_KEY Loaded: %s"), *HF_API_Key);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to load HF_API_KEY from Secrets"));
+	}
+}

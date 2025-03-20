b98db65531ed55f81857bd832449bc96805cd3ad
img98 <nichy98@naver.com>
2025-03-20 18:21:39 +0900

    [feat] : 채팅UI 블루프린트 생성

---
+++ Source/RedrumAI/Public/UI/RAIStageHUD.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RAIStageHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDRUMAI_API URAIStageHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	//virtual void NativeConstruct() override;
+
+
+};

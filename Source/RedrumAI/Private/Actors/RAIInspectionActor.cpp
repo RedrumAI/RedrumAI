7efb3f011f0c20cbc00cbc28faa8ee7a03afe4af
img98 <nichy98@naver.com>
2025-10-16 18:07:36 +0900

    [feat] : InspectionActor 생성 및 SceneCaputre기반 개별 렌더링, LightingChannels 설정
    Co-Authored-By: museongchoi <97529960+museongchoi@users.noreply.github.com>

---
+++ Source/RedrumAI/Private/Actors/RAIInspectionActor.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/RAIInspectionActor.h"
+#include "Components/PointLightComponent.h"
+#include "Components/SceneCaptureComponent2D.h"
+
+ARAIInspectionActor::ARAIInspectionActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	//각 컴포넌트 위치는 BP에서 조정
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(RootComponent);
+	PointLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLight"));
+	PointLight->SetupAttachment(RootComponent);
+	SceneCaptureComponent2D = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("SceneCaptureComponent2D"));
+	SceneCaptureComponent2D->SetupAttachment(RootComponent);
+	
+	MeshComponent->SetOnlyOwnerSee(true);
+	MeshComponent->SetLightingChannels(false, true, false);
+	PointLight->SetLightingChannels(false, true, false);
+	SceneCaptureComponent2D->ShowOnlyComponent(MeshComponent);
+	//SceneCaptureComponent2D->TextureTarget은 BP에서 할당
+}
+
+void ARAIInspectionActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ARAIInspectionActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/RedrumAI/Public/Actors/RAIInspectionActor.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RAIInspectionActor.generated.h"
+
+class UPointLightComponent;
+class USceneCaptureComponent2D;
+
+UCLASS()
+class REDRUMAI_API ARAIInspectionActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ARAIInspectionActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+	//컴포넌트 위치는 BP에서 조정
+	UPROPERTY(VisibleAnywhere, Category = "RAI")
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+	UPROPERTY(EditAnywhere, Category = "RAI")
+	TObjectPtr<UPointLightComponent> PointLight;
+	UPROPERTY(EditAnywhere, Category = "RAI")
+	TObjectPtr<USceneCaptureComponent2D> SceneCaptureComponent2D;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+};

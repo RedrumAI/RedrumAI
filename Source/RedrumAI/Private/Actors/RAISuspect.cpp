d46055b481554c3c661df1acccbed990b986322d
img98 <nichy98@naver.com>
2025-11-14 15:43:33 +0900

    [feat] : 대화이동 Interface 생성, 시야각 판단 로직 구현
    Co-Authored-By: museongchoi <97529960+museongchoi@users.noreply.github.com>

--- Source/RedrumAI/Private/Actors/RAISuspect.cpp
+++ Source/RedrumAI/Private/Actors/RAISuspect.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Actors/RAISuspect.h"
@@ -6,6 +6,13 @@
 
 ARAISuspect::ARAISuspect()
 {
+	FrontAnchor = CreateDefaultSubobject<USceneComponent>(TEXT("FrontAnchor"));
+	FrontAnchor->SetupAttachment(OriginalMesh);
+	LeftAnchor = CreateDefaultSubobject<USceneComponent>(TEXT("LeftAnchor"));
+	LeftAnchor->SetupAttachment(OriginalMesh);
+	RightAnchor = CreateDefaultSubobject<USceneComponent>(TEXT("RightAnchor"));
+	RightAnchor->SetupAttachment(OriginalMesh);
+
 	InteractType = ERAIInteractType::Suspect;
 }
 
@@ -13,4 +20,64 @@ void ARAISuspect::Interacted(AController* InController)
 {
 	ARAIPlayerController* InPC = Cast<ARAIPlayerController>(InController);
 	InPC->SetTalkingState(true);
+}
+
+ERAIConversationSide ARAISuspect::GetConversationSide(AController* InController) const
+{
+	const APlayerController* PlayerController = Cast<APlayerController>(InController);
+	ensure(PlayerController);
+
+	// 플레이어와 용의자 위치
+	const FVector PawnLocation = PlayerController->GetPawn()->GetActorLocation();
+	const FVector SuspectLocation = GetActorLocation();
+
+	// 1. 용의자 기준 "정면 시야각" 안에 카메라가 있는지 판정
+	FVector SuspectForwardVector = GetActorForwardVector();
+	SuspectForwardVector.Z = 0.f;
+	SuspectForwardVector.Normalize();
+
+	FVector SuspectToPawnVector = PawnLocation - SuspectLocation;
+	SuspectToPawnVector.Z = 0.f;
+	SuspectToPawnVector.Normalize();
+
+	const float DotForward = FVector::DotProduct(SuspectForwardVector, SuspectToPawnVector);
+
+	// VisionHalfAngle: 용의자 전방 시야각의 '절반 각도', 인터페이스 내부에 존재
+	const float CosVisionHalfAngle = FMath::Cos(FMath::DegreesToRadians(VisionHalfAngle));
+
+	if (DotForward >= CosVisionHalfAngle)
+	{
+		return ERAIConversationSide::Front;
+	}
+	else
+	{
+		// 2. 정면이 아니라면, 용의자 기준 좌/우 판정
+		FVector SuspectRightVector = GetActorRightVector();
+		SuspectRightVector.Z = 0.f;
+		SuspectRightVector.Normalize();
+
+		const float DotRight = FVector::DotProduct(SuspectRightVector, SuspectToPawnVector);
+
+		return (DotRight >= 0.f) ? ERAIConversationSide::Right : ERAIConversationSide::Left;
+	}
+}
+
+FVector ARAISuspect::GetFrontAnchorLocation() const
+{
+	return FVector();
+}
+
+FVector ARAISuspect::GetLeftAnchorLocation() const
+{
+	return FVector();
+}
+
+FVector ARAISuspect::GetRightAnchorLocation() const
+{
+	return FVector();
+}
+
+FVector ARAISuspect::GetHeadWorldLocation() const
+{
+	return FVector();
 }
---
+++ Source/RedrumAI/Public/Actors/RAIConversationInterface.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "RAIConversationInterface.generated.h"
+
+UENUM(BlueprintType)
+enum class ERAIConversationSide : uint8
+{
+	Front  UMETA(DisplayName = "Front"),
+	Left   UMETA(DisplayName = "Left"),
+	Right  UMETA(DisplayName = "Right")
+};
+
+UINTERFACE(MinimalAPI)
+class URAIConversationInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class REDRUMAI_API IRAIConversationInterface
+{
+	GENERATED_BODY()
+
+public:
+    float VisionHalfAngle = 40.f;
+
+    // 플레이어 컨트롤러를 기준으로, 이 타겟 기준 방향을 계산
+    virtual ERAIConversationSide GetConversationSide(AController* InController) const = 0;
+
+    // 각 앵커 위치
+    virtual FVector GetFrontAnchorLocation() const = 0;
+    virtual FVector GetLeftAnchorLocation()  const = 0;
+    virtual FVector GetRightAnchorLocation() const = 0;
+
+    // 카메라가 바라볼 위치(머리/목 등)
+    virtual FVector GetHeadWorldLocation() const = 0;
+
+    // (선택) 대화 시작/종료 시 훅
+    // virtual void OnConversationStarted(AController* InController) {}
+    // virtual void OnConversationFinished(AController* InController) {}
+	
+};
